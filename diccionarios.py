62db8ebfbc25b553409d39ac9b49fcadccb1ce05
yeraygit <yamestoy@hotmail.com>
2021-07-07 01:57:05 +0200

    clase 6 de junio 2021

--- diccionarios.py
+++ diccionarios.py
@@ -1,4 +1,6 @@
-lista = [{'nombre': 'admin', 'password': 'admin'},]
+import random
+
+'''lista = [{'nombre': 'admin', 'password': 'admin'},]
 lista_values = []
 while True:
     alta_usuario = input('quiere ingresar un nuevo usuario?: s/n')
@@ -15,4 +17,52 @@ while True:
         break
     else:
         print('ingrese s o n: ')
-        
+'''
+   #---------------------------------------------------------------------------
+'''
+numeros = []
+menor = 0
+mayor = 0
+for j in range(10):
+    numero_aleatorio = random.randint(1,100)
+    numeros.append(numero_aleatorio)     
+for i in range(len(numeros)):
+    if i==9:
+        break
+    elif  numeros[i]<numeros[i+1]:
+        menor=numeros[i]
+    else:
+        menor==numeros[i+1]
+print (numeros)
+print ('----------------')
+print (menor)
+#-------------------------------------------------------------------------
+tabla_diez = []
+numero_multiplicacion = int(input('digame que numero quiere multiplicar'))
+for i in range(11):
+    tabla_diez.append(numero_multiplicacion*i)
+print(tabla_diez)
+'''
+#--------------------------------------------------------------------
+
+'''lista_numeros = []
+numeros_para_ingresar = 1
+while numeros_para_ingresar!=0:
+    numeros_para_ingresar = int(input('ingrese numero para guardar, si quiere salir pulse 0'))
+    if numeros_para_ingresar!=0:
+        lista_numeros.append(numeros_para_ingresar)
+print(lista_numeros)
+'''
+#----------------------------------------------------------------------
+
+
+Luisa = {'nombre':'luisa', 'apellido':'sosa', 'sexo':'mujer'}
+Juan = {'nombre': 'juan', 'apellido': 'sosa', 'sexo':'varon'}
+Gabriel = {'nombre':'Gabriel', 'apellido':'martinez', 'sexo':'varon'}
+familia = [Luisa, Juan, Gabriel]
+for value in familia:
+    print(value)
+   
+
+#----------------------------------------------------------------------------------
+
