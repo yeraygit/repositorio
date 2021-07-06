fef0e29befc0157947f7730ea8271106c56b9438
yeraygit <yamestoy@hotmail.com>
2021-07-06 21:46:42 +0200

    nuevo usuario

--- diccionarios.py
+++ diccionarios.py
@@ -1,4 +1,18 @@
-print ('hola')
-for x in range(100):
-    print(x+2)
-    
+lista = [{'nombre': 'admin', 'password': 'admin'},]
+lista_values = []
+while True:
+    alta_usuario = input('quiere ingresar un nuevo usuario?: s/n')
+    if alta_usuario=='s':
+        nuevo_usuario= input('introduzca el nombre de usuario: ')  
+        for diccionario in lista:     
+            lista_values.append(diccionario['nombre'])
+        if nuevo_usuario in lista_values:
+            print('el nombre ya existe')
+        else:
+            nueva_contraseña= input('introduzca la contraseña. ')
+            lista.append({'nombre': nuevo_usuario, 'password':nueva_contraseña})
+    elif alta_usuario=='n':
+        break
+    else:
+        print('ingrese s o n: ')
+        
